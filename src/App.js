a57d5aedd9e3ef426aa121c9f6e8dc9885095dd8
yescine <bouchoucha.yassine@gmail.com>
2020-12-25 21:54:40 +0100

    implement simple card in container

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { Paper } from "@material-ui/core";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import TabContext from "@material-ui/lab/TabContext";
+import TabList from "@material-ui/lab/TabList";
+import { TabPanel } from "@material-ui/lab";
 
-function App() {
+// Material Icons
+import AddToQueueIcon from "@material-ui/icons/AddToQueue";
+import AirportShuttleIcon from "@material-ui/icons/AirportShuttle";
+import ApartmentIcon from "@material-ui/icons/Apartment";
+// Import View Components
+import MapView from "./views/MapBox";
+// import Container2 from "./container/Container2";
+
+export default function App() {
+  const [tab, setTab] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello CodeSandbox</h1>
+      <Paper square>
+        <TabContext value={tab}>
+          <TabList
+            onChange={(event, NewValue) => setTab(NewValue)}
+            variant="standard"
+            indicatorColor="primary"
+            textColor="primary"
+          >
+            <Tab icon={<AddToQueueIcon />} label="view 1" value={0} />
+            <Tab icon={<AirportShuttleIcon />} label="container 2" value={1} />
+            <Tab icon={<ApartmentIcon />} label="component 3" value={2} />
+          </TabList>
+          <TabPanel style={{ padding: 0, paddingTop: "0.5rem" }} value={0}>
+            <MapView />
+          </TabPanel>
+          <TabPanel value={1}>
+            <div>tab 2</div>
+          </TabPanel>
+          <TabPanel value={2}>number 4</TabPanel>
+        </TabContext>
+      </Paper>
     </div>
   );
 }
-
-export default App;
---
+++ src/views/MapBox.jsx
@@ -0,0 +1,95 @@
+import React, { useRef, useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import mapboxgl from "mapbox-gl";
+
+// Material ui core
+import Tooltip from "../components/Tooltip";
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA";
+const initiLocation = [0, 0];
+// Initialize
+navigator.geolocation.getCurrentPosition(
+  (position) => {
+    initiLocation[0] = position.coords.longitude;
+    initiLocation[1] = position.coords.latitude;
+  },
+  (err) => {},
+  { enableHighAccuracy: true }
+);
+
+const Map = () => {
+  const mapContainerRef = useRef(null);
+  const tooltipRef = useRef(new mapboxgl.Popup({ offset: 15 }));
+
+  const [long, setLng] = useState(initiLocation[0]);
+  const [lat, setLat] = useState(initiLocation[1]);
+  const [zoom, setZoom] = useState(12);
+
+  // Initialize map when component mounts
+  useEffect(() => {
+    const map = new mapboxgl.Map(
+      {
+        container: mapContainerRef.current,
+        style: "mapbox://styles/mapbox/streets-v11",
+        center: [long, lat],
+        zoom: zoom
+      },
+      []
+    );
+
+    // Add navigation control (the +/- zoom buttons)
+    map.addControl(new mapboxgl.NavigationControl(), "top-right");
+
+    map.on("move", () => {
+      setLng(map.getCenter().lng.toFixed(4));
+      setLat(map.getCenter().lat.toFixed(4));
+      setZoom(map.getZoom().toFixed(2));
+    });
+
+    // change cursor to pointer when user hovers over a clickable feature
+    map.on("mouseenter", (e) => {
+      if (e.features.length) {
+        map.getCanvas().style.cursor = "pointer";
+      }
+    });
+
+    // reset cursor to default when user is no longer hovering over a clickable feature
+    map.on("mouseleave", () => {
+      map.getCanvas().style.cursor = "";
+    });
+
+    // add tooltip when users mouse move over a point
+   //  map.on("mousemove", (e) => {
+   //    const features = map.queryRenderedFeatures(e.point);
+   //    if (features.length) {
+   //      const feature = features[0];
+
+   //      // Create tooltip node
+   //      const TooltipNode = document.createElement("div");
+   //      ReactDOM.render(<Tooltip feature={feature} />, TooltipNode);
+
+   //      // Set tooltip on map
+   //      tooltipRef.current
+   //        .setLngLat(e.lngLat)
+   //        .setDOMContent(TooltipNode)
+   //        .addTo(map);
+   //    }
+   //  });
+
+    // Clean up on unmount
+    return () => map.remove();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <div>
+      <div className="sidebarStyle">
+        <div>
+          Longitude: {long} | Latitude: {lat} | Zoom: {zoom}
+        </div>
+      </div>
+      <div style={{height:'25rem'}} ref={mapContainerRef} />
+    </div>
+  );
+};
+
+export default Map;
